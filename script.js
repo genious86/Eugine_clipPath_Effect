2a28b2e8ac800e2685167e6ca08513afa1a61254
genious86 <pauleugenius86@gmail.com>
2024-09-20 07:58:13 +0300

    declaring variables adding eventlistener and a callback function

---
+++ script.js
@@ -0,0 +1,6 @@
+let circle1 = document.querySelector('.circle1')
+let circle2 = document.querySelector('.circle2')
+
+window.addEventListener('scroll', ()=>{
+    let scrollValue =window.scrollY
+})
